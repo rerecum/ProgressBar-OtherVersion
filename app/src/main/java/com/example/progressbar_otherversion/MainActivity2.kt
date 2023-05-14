f6e7c7283657898c4ccbe2e3bf314798204933db
rerecum <kacperkacperkacper132@gmail.com>
2023-05-14 15:20:27 +0200

    Done hole progress bar programme Version 2

---
+++ app/src/main/java/com/example/progressbar_otherversion/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.progressbar_otherversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val message = intent.getStringExtra(AlarmClock.EXTRA_MESSAGE)
+        textView.text = message
+    }
+}
